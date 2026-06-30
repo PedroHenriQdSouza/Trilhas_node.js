d713ce22555e3ec32aabf87429c3d5638837ebc1
PedroHenriQdSouza <118356675+PedroHenriQdSouza@users.noreply.github.com>
2026-06-30 10:07:06 -0300

    continuar trilha 13

--- atividades nodes.js/apiRest/index.js
+++ atividades nodes.js/apiRest/index.js
@@ -1,23 +1,31 @@
 import express from 'express';
 import colecaoUf from './dados/dados.js';
 
-const app = express ();
+const app = express();
 
 app.get('/ufs', (req, res) => {
     res.json(colecaoUf);
 });
 
-app.get('/ufs/:iduf', (req, res) =>{
-    const iduf = parseInt(req.params.iduf);
-    const uf = colecaoUf.find((uf) => uf.id === iduf);
-    
-    if (uf){
-        res.json(uf);       
-    }else{
-        res.status(404).send();
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find((uf) => uf.id === idUF)
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+    } else {
+        mensagemErro = 'requisição inválida';
     }
 
-    res.json(uf)
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).send({"erro": mensagemErro});
+    }
 });
 app.listen(8000, () => {
     console.log('Servidor rodando na porta 8000');
